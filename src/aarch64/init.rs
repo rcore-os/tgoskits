kylin-x-kernel/axcpu

refactor: re-organize uspace export

src/aarch64/init.rs
@@ -99,7 +99,8 @@ pub unsafe fn init_mmu(root_paddr: PhysAddr) {
 /// In detail, it initializes the exception vector, and sets `TTBR0_EL1` to 0 to
 /// block low address access.
 pub fn init_trap() {
-    crate::trap::init_exception_table();
+    #[cfg(feature = "uspace")]
+    crate::uspace::init_exception_table();
     unsafe extern "C" {
         fn exception_vector_base();
     }

src/loongarch64/init.rs
@@ -40,7 +40,8 @@ pub fn init_mmu(root_paddr: PhysAddr, phys_virt_offset: usize) {
 ///
 /// In detail, it initializes the exception vector on LoongArch64 platforms.
 pub fn init_trap() {
-    crate::trap::init_exception_table();
+    #[cfg(feature = "uspace")]
+    crate::uspace::init_exception_table();
     unsafe {
         extern "C" {
             fn exception_entry_base();

src/riscv/init.rs
@@ -4,7 +4,8 @@
 ///
 /// In detail, it initializes the trap vector on RISC-V platforms.
 pub fn init_trap() {
-    crate::trap::init_exception_table();
+    #[cfg(feature = "uspace")]
+    crate::uspace::init_exception_table();
     unsafe extern "C" {
         fn trap_vector_base();
     }

src/x86_64/init.rs
@@ -31,7 +31,8 @@ pub fn init_percpu(cpu_id: usize) {
 ///
 /// [`percpu`]: https://docs.rs/percpu/latest/percpu/index.html
 pub fn init_trap() {
-    crate::trap::init_exception_table();
+    #[cfg(feature = "uspace")]
+    crate::uspace::init_exception_table();
     init_gdt();
     init_idt();
     #[cfg(feature = "uspace")]

rcore-os/tgoskits

src/aarch64/init.rs
@@ -99,7 +99,8 @@ pub unsafe fn init_mmu(root_paddr: PhysAddr) {
 /// In detail, it initializes the exception vector, and sets `TTBR0_EL1` to 0 to
 /// block low address access.
 pub fn init_trap() {
-    crate::trap::init_exception_table();
+    #[cfg(feature = "uspace")]
+    crate::uspace_common::init_exception_table();
     unsafe extern "C" {
         fn exception_vector_base();
     }

src/loongarch64/init.rs
@@ -40,7 +40,8 @@ pub fn init_mmu(root_paddr: PhysAddr, phys_virt_offset: usize) {
 ///
 /// In detail, it initializes the exception vector on LoongArch64 platforms.
 pub fn init_trap() {
-    crate::trap::init_exception_table();
+    #[cfg(feature = "uspace")]
+    crate::uspace_common::init_exception_table();
     unsafe {
         extern "C" {
             fn exception_entry_base();

src/riscv/init.rs
@@ -4,7 +4,8 @@
 ///
 /// In detail, it initializes the trap vector on RISC-V platforms.
 pub fn init_trap() {
-    crate::trap::init_exception_table();
+    #[cfg(feature = "uspace")]
+    crate::uspace_common::init_exception_table();
     unsafe extern "C" {
         fn trap_vector_base();
     }

src/x86_64/init.rs
@@ -31,7 +31,8 @@ pub fn init_percpu(cpu_id: usize) {
 ///
 /// [`percpu`]: https://docs.rs/percpu/latest/percpu/index.html
 pub fn init_trap() {
-    crate::trap::init_exception_table();
+    #[cfg(feature = "uspace")]
+    crate::uspace_common::init_exception_table();
     init_gdt();
     init_idt();
     #[cfg(feature = "uspace")]