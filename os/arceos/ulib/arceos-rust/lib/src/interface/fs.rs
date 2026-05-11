Shl1015CS/ArceOS

feat[rust-std]: add basic fs support

ulib/arceos-rust/lib/src/interface/fs.rs
@@ -1,0 +1,53 @@
+use crate::err;
+use arceos_posix_api::ctypes::stat;
+use arceos_posix_api::utils::char_ptr_to_str;
+use axerrno::AxErrorKind::IsADirectory;
+use core::ffi::c_char;
+use log::info;
+
+#[unsafe(no_mangle)]
+pub fn sys_stat(name: *const c_char, stat: *mut stat) -> i32 {
+    info!("called sys_stat");
+    unsafe { arceos_posix_api::sys_stat(name, stat as _) }
+}
+
+#[unsafe(no_mangle)]
+pub fn sys_lstat(name: *const c_char, stat: *mut stat) -> i32 {
+    info!("called sys_lstat");
+    unsafe { arceos_posix_api::sys_lstat(name, stat as _) as _ }
+}
+
+#[unsafe(no_mangle)]
+pub fn sys_fstat(fd: i32, stat: *mut stat) -> i32 {
+    info!("called sys_fstat");
+    unsafe { arceos_posix_api::sys_fstat(fd, stat as _) as _ }
+}
+
+#[unsafe(no_mangle)]
+pub fn sys_unlink(name: *const c_char) -> i32 {
+    info!("called sys_unlink");
+    // get name as &str
+    let name = match char_ptr_to_str(name) {
+        Ok(s) => s,
+        Err(e) => return err(e),
+    };
+
+    if let Err(IsADirectory) = arceos_api::fs::ax_remove_file(name) {
+        if let Err(e) = arceos_api::fs::ax_remove_dir(name) {
+            return err(e.into());
+        }
+    }
+    0
+}
+
+#[unsafe(no_mangle)]
+pub fn sys_mkdir(name: *const i8, _mode: u32) -> i32 {
+    let name = match char_ptr_to_str(name) {
+        Ok(s) => s,
+        Err(e) => return err(e),
+    };
+    if let Err(e) = arceos_api::fs::ax_create_dir(name) {
+        return err(e.into());
+    }
+    0
+}

ulib/arceos-rust/lib/src/interface/io.rs
@@ -1,4 +1,5 @@
 use arceos_posix_api::ctypes::iovec;
+use core::ffi::c_char;
 use log::info;
 
 #[unsafe(no_mangle)]
@@ -18,4 +19,24 @@ pub fn sys_writev(fd: i32, iov: *const iovec, iovcnt: usize) -> isize {
 pub fn sys_close(fd: i32) -> i32 {
     info!("called sys_close");
     arceos_posix_api::sys_close(fd) as _
+}
+
+#[unsafe(no_mangle)]
+pub fn sys_read(fd: i32, buf: *mut u8, len: usize) -> isize {
+    info!("called sys_read");
+    arceos_posix_api::sys_read(fd, buf as _, len)
+}
+
+#[cfg(feature = "fs")]
+#[unsafe(no_mangle)]
+pub fn sys_lseek(fd: i32, offset: isize, whence: i32) -> isize {
+    info!("called sys_lseek");
+    arceos_posix_api::sys_lseek(fd, offset as _, whence) as _
+}
+
+#[cfg(feature = "fs")]
+#[unsafe(no_mangle)]
+pub fn sys_open(name: *const c_char, flags: i32, mode: i32) -> i32 {
+    info!("called sys_open");
+    arceos_posix_api::sys_open(name, flags, mode as _)
 }

rcore-os/tgoskits

os/arceos/ulib/arceos-rust/lib/src/interface/fs.rs
@@ -1,0 +1,53 @@
+use crate::err;
+use ax_posix_api::ctypes::stat;
+use ax_posix_api::utils::char_ptr_to_str;
+use axerrno::AxErrorKind::IsADirectory;
+use core::ffi::c_char;
+use log::info;
+
+#[unsafe(no_mangle)]
+pub fn sys_stat(name: *const c_char, stat: *mut stat) -> i32 {
+    info!("called sys_stat");
+    unsafe { ax_posix_api::sys_stat(name, stat as _) }
+}
+
+#[unsafe(no_mangle)]
+pub fn sys_lstat(name: *const c_char, stat: *mut stat) -> i32 {
+    info!("called sys_lstat");
+    unsafe { ax_posix_api::sys_lstat(name, stat as _) as _ }
+}
+
+#[unsafe(no_mangle)]
+pub fn sys_fstat(fd: i32, stat: *mut stat) -> i32 {
+    info!("called sys_fstat");
+    unsafe { ax_posix_api::sys_fstat(fd, stat as _) as _ }
+}
+
+#[unsafe(no_mangle)]
+pub fn sys_unlink(name: *const c_char) -> i32 {
+    info!("called sys_unlink");
+    // get name as &str
+    let name = match char_ptr_to_str(name) {
+        Ok(s) => s,
+        Err(e) => return err(e),
+    };
+
+    if let Err(IsADirectory) = ax_api::fs::ax_remove_file(name) {
+        if let Err(e) = ax_api::fs::ax_remove_dir(name) {
+            return err(e.into());
+        }
+    }
+    0
+}
+
+#[unsafe(no_mangle)]
+pub fn sys_mkdir(name: *const i8, _mode: u32) -> i32 {
+    let name = match char_ptr_to_str(name) {
+        Ok(s) => s,
+        Err(e) => return err(e),
+    };
+    if let Err(e) = ax_api::fs::ax_create_dir(name) {
+        return err(e.into());
+    }
+    0
+}

os/arceos/ulib/arceos-rust/lib/src/interface/io.rs
@@ -1,4 +1,5 @@
 use ax_posix_api::ctypes::iovec;
+use core::ffi::c_char;
 use log::info;
 
 #[unsafe(no_mangle)]
@@ -18,4 +19,24 @@ pub fn sys_writev(fd: i32, iov: *const iovec, iovcnt: usize) -> isize {
 pub fn sys_close(fd: i32) -> i32 {
     info!("called sys_close");
     ax_posix_api::sys_close(fd) as _
+}
+
+#[unsafe(no_mangle)]
+pub fn sys_read(fd: i32, buf: *mut u8, len: usize) -> isize {
+    info!("called sys_read");
+    ax_posix_api::sys_read(fd, buf as _, len)
+}
+
+#[cfg(feature = "fs")]
+#[unsafe(no_mangle)]
+pub fn sys_lseek(fd: i32, offset: isize, whence: i32) -> isize {
+    info!("called sys_lseek");
+    ax_posix_api::sys_lseek(fd, offset as _, whence) as _
+}
+
+#[cfg(feature = "fs")]
+#[unsafe(no_mangle)]
+pub fn sys_open(name: *const c_char, flags: i32, mode: i32) -> i32 {
+    info!("called sys_open");
+    ax_posix_api::sys_open(name, flags, mode as _)
 }