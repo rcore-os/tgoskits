Shl1015CS/ArceOS

feat[rust-std]: add support for network

ulib/arceos-rust/lib/src/lib.rs
@@ -2,8 +2,9 @@
 extern crate alloc;
 
 mod syscall;
+mod interface;
 
-use arceos_api::modules::axlog::{ax_println, debug, info};
+use arceos_api::modules::axlog::{debug, info};
 pub use axruntime;
 
 unsafe extern "C" {

rcore-os/tgoskits

os/arceos/ulib/arceos-rust/lib/src/lib.rs
@@ -4,8 +4,9 @@
 extern crate alloc;
 
 mod syscall;
+mod interface;
 
-use ax_api::modules::ax_log::{ax_println, debug, info};
+use ax_api::modules::ax_log::{debug, info};
 pub use ax_runtime;
 
 unsafe extern "C" {