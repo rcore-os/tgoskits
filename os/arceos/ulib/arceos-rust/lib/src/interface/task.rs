Shl1015CS/ArceOS

refactor[rust-std]: move task related interfaces to a new module

ulib/arceos-rust/lib/src/interface/task.rs
@@ -1,0 +1,15 @@
+use arceos_api::modules::axlog::{ax_println, info};
+use arceos_api::sys::ax_terminate;
+
+#[unsafe(no_mangle)]
+pub fn sys_abort() -> ! {
+    info!("called sys_abort");
+    ax_terminate()
+}
+
+#[unsafe(no_mangle)]
+pub fn sys_exit(code: i32) -> ! {
+    info!("called sys_exit with code {}", code);
+    ax_println!("[ArceOS] Process exited with code {}", code);
+    ax_terminate()
+}

rcore-os/tgoskits

os/arceos/ulib/arceos-rust/lib/src/interface/task.rs
@@ -1,0 +1,15 @@
+use ax_api::modules::ax_log::{ax_println, info};
+use ax_api::sys::ax_terminate;
+
+#[unsafe(no_mangle)]
+pub fn sys_abort() -> ! {
+    info!("called sys_abort");
+    ax_terminate()
+}
+
+#[unsafe(no_mangle)]
+pub fn sys_exit(code: i32) -> ! {
+    info!("called sys_exit with code {}", code);
+    ax_println!("[ArceOS] Process exited with code {}", code);
+    ax_terminate()
+}