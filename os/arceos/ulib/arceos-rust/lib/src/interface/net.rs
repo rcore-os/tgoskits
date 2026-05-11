Shl1015CS/ArceOS

feat[rust-std]: add socket-related interfaces

ulib/arceos-rust/lib/src/interface/net.rs
@@ -1,5 +1,5 @@
 use arceos_api::modules::axlog::info;
-use arceos_posix_api::ctypes::{addrinfo, sockaddr, socklen_t, AF_INET, AF_INET6};
+use arceos_posix_api::ctypes::{addrinfo, sockaddr, socklen_t};
 use core::ffi::{c_char, c_void};
 
 #[unsafe(no_mangle)]
@@ -15,7 +15,10 @@ pub fn sys_getaddrinfo(
     hints: *const addrinfo,
     res: *mut *mut addrinfo,
 ) -> i32 {
-    info!("[sys_getaddrinfo] nodename: {:?}, servname: {:?}", nodename, servname);
+    info!(
+        "[sys_getaddrinfo] nodename: {:?}, servname: {:?}",
+        nodename, servname
+    );
     let result = unsafe { arceos_posix_api::sys_getaddrinfo(nodename, servname, hints, res) };
     if result > 0 {
         // hermit expected us to return 0 if success
@@ -57,4 +60,52 @@ pub fn sys_recv(socket: i32, buf: *mut u8, len: usize, flags: i32) -> isize {
         socket, len, flags
     );
     arceos_posix_api::sys_recv(socket, buf as _, len, flags)
+}
+
+#[unsafe(no_mangle)]
+pub fn sys_listen(socket_fd: i32, backlog: i32) -> i32 {
+    info!(
+        "[sys_listen] socket_fd: {}, backlog: {}",
+        socket_fd, backlog
+    );
+    arceos_posix_api::sys_listen(socket_fd, backlog)
+}
+
+#[unsafe(no_mangle)]
+pub fn sys_bind(socket_fd: i32, name: *const sockaddr, namelen: socklen_t) -> i32 {
+    info!("[sys_bind] socket_fd: {}, namelen: {}", socket_fd, namelen);
+    arceos_posix_api::sys_bind(socket_fd, name, namelen)
+}
+
+#[unsafe(no_mangle)]
+pub fn sys_getsockname(socket_fd: i32, name: *mut sockaddr, namelen: *mut socklen_t) -> i32 {
+    info!(
+        "[sys_getsockname] socket_fd: {}, namelen: {:p}",
+        socket_fd, namelen
+    );
+    unsafe { arceos_posix_api::sys_getsockname(socket_fd, name, namelen) }
+}
+
+#[unsafe(no_mangle)]
+pub fn sys_accept(socket_fd: i32, addr: *mut sockaddr, addrlen: *mut socklen_t) -> i32 {
+    info!(
+        "[sys_accept] socket_fd: {}, addrlen: {:p}",
+        socket_fd, addrlen
+    );
+    unsafe { arceos_posix_api::sys_accept(socket_fd, addr, addrlen) }
+}
+
+#[unsafe(no_mangle)]
+pub fn sys_setsockopt(
+    socket_fd: i32,
+    level: i32,
+    optname: i32,
+    optval: *const c_void,
+    optlen: socklen_t,
+) -> i32 {
+    info!(
+        "[setsockopt] socket_fd: {}, level: {}, optname: {}, optlen: {}",
+        socket_fd, level, optname, optlen
+    );
+    unsafe { arceos_posix_api::sys_setsockopt(socket_fd, level, optname, optval, optlen) }
 }

rcore-os/tgoskits

os/arceos/ulib/arceos-rust/lib/src/interface/net.rs
@@ -1,5 +1,5 @@
 use ax_api::modules::ax_log::info;
-use ax_posix_api::ctypes::{addrinfo, sockaddr, socklen_t, AF_INET, AF_INET6};
+use ax_posix_api::ctypes::{addrinfo, sockaddr, socklen_t};
 use core::ffi::{c_char, c_void};
 
 #[unsafe(no_mangle)]
@@ -15,7 +15,10 @@ pub fn sys_getaddrinfo(
     hints: *const addrinfo,
     res: *mut *mut addrinfo,
 ) -> i32 {
-    info!("[sys_getaddrinfo] nodename: {:?}, servname: {:?}", nodename, servname);
+    info!(
+        "[sys_getaddrinfo] nodename: {:?}, servname: {:?}",
+        nodename, servname
+    );
     let result = unsafe { ax_posix_api::sys_getaddrinfo(nodename, servname, hints, res) };
     if result > 0 {
         // hermit expected us to return 0 if success
@@ -57,4 +60,52 @@ pub fn sys_recv(socket: i32, buf: *mut u8, len: usize, flags: i32) -> isize {
         socket, len, flags
     );
     ax_posix_api::sys_recv(socket, buf as _, len, flags)
+}
+
+#[unsafe(no_mangle)]
+pub fn sys_listen(socket_fd: i32, backlog: i32) -> i32 {
+    info!(
+        "[sys_listen] socket_fd: {}, backlog: {}",
+        socket_fd, backlog
+    );
+    arceos_posix_api::sys_listen(socket_fd, backlog)
+}
+
+#[unsafe(no_mangle)]
+pub fn sys_bind(socket_fd: i32, name: *const sockaddr, namelen: socklen_t) -> i32 {
+    info!("[sys_bind] socket_fd: {}, namelen: {}", socket_fd, namelen);
+    arceos_posix_api::sys_bind(socket_fd, name, namelen)
+}
+
+#[unsafe(no_mangle)]
+pub fn sys_getsockname(socket_fd: i32, name: *mut sockaddr, namelen: *mut socklen_t) -> i32 {
+    info!(
+        "[sys_getsockname] socket_fd: {}, namelen: {:p}",
+        socket_fd, namelen
+    );
+    unsafe { arceos_posix_api::sys_getsockname(socket_fd, name, namelen) }
+}
+
+#[unsafe(no_mangle)]
+pub fn sys_accept(socket_fd: i32, addr: *mut sockaddr, addrlen: *mut socklen_t) -> i32 {
+    info!(
+        "[sys_accept] socket_fd: {}, addrlen: {:p}",
+        socket_fd, addrlen
+    );
+    unsafe { arceos_posix_api::sys_accept(socket_fd, addr, addrlen) }
+}
+
+#[unsafe(no_mangle)]
+pub fn sys_setsockopt(
+    socket_fd: i32,
+    level: i32,
+    optname: i32,
+    optval: *const c_void,
+    optlen: socklen_t,
+) -> i32 {
+    info!(
+        "[setsockopt] socket_fd: {}, level: {}, optname: {}, optlen: {}",
+        socket_fd, level, optname, optlen
+    );
+    unsafe { arceos_posix_api::sys_setsockopt(socket_fd, level, optname, optval, optlen) }
 }