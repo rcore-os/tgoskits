Shl1015CS/ArceOS

feat[rust-std]: thread and sync support

ulib/arceos-rust/lib/src/lib.rs
@@ -4,10 +4,15 @@ extern crate alloc;
 mod interface;
 
 use arceos_api::modules::axlog::{debug, info};
+use axerrno::LinuxError;
 pub use axruntime;
 
 unsafe extern "C" {
     fn runtime_entry(argc: i32, argv: *const *const u8, env: *const *const u8) -> !;
+}
+
+pub(crate) fn err(error: LinuxError) -> i32 {
+    -(error as i32)
 }
 
 #[unsafe(no_mangle)]

rcore-os/tgoskits

os/arceos/ulib/arceos-rust/lib/src/lib.rs
@@ -6,10 +6,15 @@ extern crate alloc;
 mod interface;
 
 use ax_api::modules::ax_log::{debug, info};
+use ax_errno::LinuxError;
 pub use ax_runtime;
 
 unsafe extern "C" {
     fn runtime_entry(argc: i32, argv: *const *const u8, env: *const *const u8) -> !;
+}
+
+pub(crate) fn err(error: LinuxError) -> i32 {
+    -(error as i32)
 }
 
 #[eii(ax_app_entry)]