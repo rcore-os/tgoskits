livstyle/tgoskits-xuanshu

feat(x86_64): add x2APIC support and non-identity guest memory for AxVisor

virtualization/x86_vcpu/src/vmx/percpu.rs
@@ -82,48 +82,54 @@ impl VmArchPerCpuOps for VmxPerCpuState {
             return ax_err!(Unsupported, "VMX disabled by BIOS");
         }
 
-        // Check control registers are in a VMX-friendly state. (SDM Vol. 3C, Appendix A.7, A.8)
-        macro_rules! cr_is_valid {
-            ($value:expr, $crx:ident) => {{
-                use Msr::*;
-                let value = $value;
-                paste::paste! {
-                    let fixed0 = [<IA32_VMX_ $crx _FIXED0>].read();
-                    let fixed1 = [<IA32_VMX_ $crx _FIXED1>].read();
-                }
-                (!fixed0 | value != 0) && (fixed1 | !value != 0)
-            }};
-        }
-        if !cr_is_valid!(Cr0::read().bits(), CR0) {
+        let cr0 = Cr0::read_raw();
+        let cr0_fixed0 = Msr::IA32_VMX_CR0_FIXED0.read();
+        let cr0_fixed1 = Msr::IA32_VMX_CR0_FIXED1.read();
+        let cr0_vmx = vmx_fixed_control_value(cr0, cr0_fixed0, cr0_fixed1);
+        let cr4 = Cr4::read_raw();
+        let cr4_fixed0 = Msr::IA32_VMX_CR4_FIXED0.read();
+        let cr4_fixed1 = Msr::IA32_VMX_CR4_FIXED1.read();
+        let cr4_vmx = vmx_fixed_control_value(cr4, cr4_fixed0, cr4_fixed1);
+
+        if !is_vmx_fixed_control_value_valid(cr0_vmx, cr0_fixed0, cr0_fixed1) {
             return ax_err!(BadState, "host CR0 is not valid in VMX operation");
         }
-        if !cr_is_valid!(Cr4::read().bits(), CR4) {
+        if !is_vmx_fixed_control_value_valid(cr4_vmx, cr4_fixed0, cr4_fixed1) {
             return ax_err!(BadState, "host CR4 is not valid in VMX operation");
         }
 
         // Get VMCS revision identifier in IA32_VMX_BASIC MSR.
         let vmx_basic = VmxBasic::read();
-        if vmx_basic.region_size as usize != PAGE_SIZE {
-            return ax_err!(Unsupported);
+        if vmx_basic.region_size as usize > PAGE_SIZE {
+            return ax_err!(
+                Unsupported,
+                format_args!(
+                    "unsupported VMX region size: {} bytes",
+                    vmx_basic.region_size
+                )
+            );
         }
         if vmx_basic.mem_type != VmxBasic::VMX_MEMORY_TYPE_WRITE_BACK {
-            return ax_err!(Unsupported);
+            return ax_err!(
+                Unsupported,
+                format_args!("unsupported VMX memory type: {}", vmx_basic.mem_type)
+            );
         }
         if vmx_basic.is_32bit_address {
-            return ax_err!(Unsupported);
+            return ax_err!(Unsupported, "unsupported 32-bit VMX physical address width");
         }
         if !vmx_basic.io_exit_info {
-            return ax_err!(Unsupported);
+            return ax_err!(Unsupported, "VMX lacks I/O exit instruction info");
         }
         if !vmx_basic.vmx_flex_controls {
-            return ax_err!(Unsupported);
+            return ax_err!(Unsupported, "VMX lacks flexible controls");
         }
         self.vmcs_revision_id = vmx_basic.revision_id;
         self.vmx_region = VmxRegion::new(self.vmcs_revision_id, false)?;
 
         unsafe {
-            // Enable VMX using the VMXE bit.
-            Cr4::write(Cr4::read() | Cr4Flags::VIRTUAL_MACHINE_EXTENSIONS);
+            Cr0::write_raw(cr0_vmx);
+            Cr4::write_raw(cr4_vmx);
             // Execute VMXON.
             vmx::vmxon(self.vmx_region.phys_addr().as_usize() as _).map_err(|err| {
                 ax_err_type!(
@@ -158,6 +164,14 @@ impl VmArchPerCpuOps for VmxPerCpuState {
         self.vmx_region = unsafe { VmxRegion::uninit() };
         Ok(())
     }
+}
+
+fn vmx_fixed_control_value(value: u64, fixed0: u64, fixed1: u64) -> u64 {
+    (value | fixed0) & fixed1
+}
+
+fn is_vmx_fixed_control_value_valid(value: u64, fixed0: u64, fixed1: u64) -> bool {
+    (value & fixed0) == fixed0 && (value & !fixed1) == 0
 }
 
 #[cfg(test)]

virtualization/x86_vcpu/src/vmx/vcpu.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use alloc::collections::VecDeque;
+use alloc::{collections::VecDeque, vec::Vec};
 use core::{
     arch::naked_asm,
     fmt::{Debug, Formatter, Result},
@@ -47,8 +47,8 @@ use super::{
     },
 };
 use crate::{
-    X86VCpuSetupConfig, ept::GuestPageWalkInfo, host, msr::Msr, regs::GeneralRegisters,
-    restore_host_interrupt_flag, x86_real_mode_entry_state, xstate::XState,
+    X86GuestMemoryRegion, X86VCpuSetupConfig, ept::GuestPageWalkInfo, host, msr::Msr,
+    regs::GeneralRegisters, restore_host_interrupt_flag, x86_real_mode_entry_state, xstate::XState,
 };
 
 const VMX_PREEMPTION_TIMER_SET_VALUE: u32 = 100_000;
@@ -132,6 +132,8 @@ pub struct VmxVcpu {
     vlapic: EmulatedLocalApic,
     /// Guest CR2 is not saved or restored by VMX hardware.
     guest_cr2: usize,
+    /// Guest RAM regions used to read guest instructions and page tables.
+    guest_memory_regions: Vec<X86GuestMemoryRegion>,
 
     // Extra states
     /// The XState of the VCpu. Both host and guest.
@@ -161,6 +163,7 @@ impl VmxVcpu {
             pending_events: VecDeque::with_capacity(8),
             vlapic: EmulatedLocalApic::new(vm_id, vcpu_id),
             guest_cr2: 0,
+            guest_memory_regions: Vec::new(),
             xstate: XState::new(),
             #[cfg(feature = "tracing")]
             guest_regs_exiting: GeneralRegisters::default(),
@@ -520,6 +523,7 @@ impl VmxVcpu {
         ept_root: HostPhysAddr,
         config: X86VCpuSetupConfig,
     ) -> AxResult {
+        self.guest_memory_regions = config.guest_memory_regions.clone();
         let paddr = self.vmcs.phys_addr().as_usize() as u64;
         unsafe {
             vmx::vmclear(paddr).map_err(as_axerr)?;
@@ -634,7 +638,7 @@ impl VmxVcpu {
         is_guest: bool,
         config: X86VCpuSetupConfig,
     ) -> AxResult {
-        // Intercept NMI and external interrupts.
+        // Intercept external interrupts and use the VMX preemption timer.
         use PinbasedControls as PinCtrl;
 
         use super::vmcs::controls::*;
@@ -644,10 +648,7 @@ impl VmxVcpu {
             VmcsControl32::PINBASED_EXEC_CONTROLS,
             Msr::IA32_VMX_TRUE_PINBASED_CTLS,
             Msr::IA32_VMX_PINBASED_CTLS.read() as u32,
-            (PinCtrl::NMI_EXITING
-                | PinCtrl::EXTERNAL_INTERRUPT_EXITING
-                | PinCtrl::VMX_PREEMPTION_TIMER)
-                .bits(),
+            (PinCtrl::EXTERNAL_INTERRUPT_EXITING | PinCtrl::VMX_PREEMPTION_TIMER).bits(),
             0,
         )?;
 
@@ -752,8 +753,8 @@ impl VmxVcpu {
         // VmcsControlNW::CR4_GUEST_HOST_MASK.write(0)?;
         VmcsControl32::CR3_TARGET_COUNT.write(0)?;
 
-        // Pass-through exceptions (except #UD(6)), don't use I/O bitmap, set MSR bitmaps.
-        let exception_bitmap: u32 = 1 << 6;
+        // Pass through guest exceptions. Linux uses #UD (`ud2`) for BUG/WARN handling.
+        let exception_bitmap: u32 = 0;
 
         self.setup_io_bitmap(config)?;
 
@@ -851,15 +852,6 @@ impl VmxVcpu {
         })()
         .expect("Failed to read guest control register")
     }
-}
-
-// The current VMX APIC-access decode path is used only with Axvisor's
-// identity-mapped guest RAM layout, so the guest physical address is also
-// the host physical address. A non-identity guest memory backend should
-// replace this helper with an explicit GPA-to-HVA translation.
-fn read_guest_phys_u64(gpa: usize) -> u64 {
-    let hva = host::phys_to_virt(HostPhysAddr::from(gpa));
-    unsafe { core::ptr::read_unaligned(hva.as_ptr() as *const u64) }
 }
 
 /// Get ready then vmlaunch or vmresume.
@@ -1312,8 +1304,42 @@ impl VmxVcpu {
 
     fn read_guest_u8(&self, gva: GuestVirtAddr) -> AxResult<u8> {
         let gpa = self.translate_guest_linear(gva)?;
-        let hva = host::phys_to_virt(HostPhysAddr::from(gpa.as_usize()));
-        Ok(unsafe { core::ptr::read_volatile(hva.as_ptr()) })
+        self.read_guest_phys_u8(gpa)
+    }
+
+    fn guest_phys_to_host_virt(&self, gpa: GuestPhysAddr) -> AxResult<usize> {
+        let gpa = gpa.as_usize();
+        if self.guest_memory_regions.is_empty() {
+            return Ok(host::phys_to_virt(HostPhysAddr::from(gpa)).as_usize());
+        }
+
+        self.guest_memory_regions
+            .iter()
+            .find_map(|region| {
+                let start = region.gpa.as_usize();
+                let offset = gpa.checked_sub(start)?;
+                if offset < region.size {
+                    region.hva.as_usize().checked_add(offset)
+                } else {
+                    None
+                }
+            })
+            .ok_or_else(|| {
+                ax_err_type!(
+                    InvalidInput,
+                    format_args!("guest physical address {gpa:#x} is not backed by RAM")
+                )
+            })
+    }
+
+    fn read_guest_phys_u8(&self, gpa: GuestPhysAddr) -> AxResult<u8> {
+        let hva = self.guest_phys_to_host_virt(gpa)?;
+        Ok(unsafe { core::ptr::read_volatile(hva as *const u8) })
+    }
+
+    fn read_guest_phys_u64(&self, gpa: GuestPhysAddr) -> AxResult<u64> {
+        let hva = self.guest_phys_to_host_virt(gpa)?;
+        Ok(unsafe { core::ptr::read_unaligned(hva as *const u64) })
     }
 
     fn translate_guest_linear(&self, gva: GuestVirtAddr) -> AxResult<GuestPhysAddr> {
@@ -1345,7 +1371,8 @@ impl VmxVcpu {
         ];
 
         for (level, index) in indexes.into_iter().enumerate() {
-            let entry = read_guest_phys_u64(table + index * size_of::<u64>());
+            let entry =
+                self.read_guest_phys_u64(GuestPhysAddr::from(table + index * size_of::<u64>()))?;
             if entry & PRESENT == 0 {
                 return ax_err!(
                     InvalidInput,

rcore-os/tgoskits

virtualization/x86_vcpu/src/vmx/percpu.rs
@@ -85,48 +85,54 @@ impl<H: X86HostOps> VmxPerCpuState<H> {
             return x86_err!(Unsupported, "VMX disabled by BIOS");
         }
 
-        // Check control registers are in a VMX-friendly state. (SDM Vol. 3C, Appendix A.7, A.8)
-        macro_rules! cr_is_valid {
-            ($value:expr, $crx:ident) => {{
-                use Msr::*;
-                let value = $value;
-                paste::paste! {
-                    let fixed0 = [<IA32_VMX_ $crx _FIXED0>].read();
-                    let fixed1 = [<IA32_VMX_ $crx _FIXED1>].read();
-                }
-                (!fixed0 | value != 0) && (fixed1 | !value != 0)
-            }};
-        }
-        if !cr_is_valid!(Cr0::read().bits(), CR0) {
+        let cr0 = Cr0::read_raw();
+        let cr0_fixed0 = Msr::IA32_VMX_CR0_FIXED0.read();
+        let cr0_fixed1 = Msr::IA32_VMX_CR0_FIXED1.read();
+        let cr0_vmx = vmx_fixed_control_value(cr0, cr0_fixed0, cr0_fixed1);
+        let cr4 = Cr4::read_raw();
+        let cr4_fixed0 = Msr::IA32_VMX_CR4_FIXED0.read();
+        let cr4_fixed1 = Msr::IA32_VMX_CR4_FIXED1.read();
+        let cr4_vmx = vmx_fixed_control_value(cr4, cr4_fixed0, cr4_fixed1);
+
+        if !is_vmx_fixed_control_value_valid(cr0_vmx, cr0_fixed0, cr0_fixed1) {
             return x86_err!(BadState, "host CR0 is not valid in VMX operation");
         }
-        if !cr_is_valid!(Cr4::read().bits(), CR4) {
+        if !is_vmx_fixed_control_value_valid(cr4_vmx, cr4_fixed0, cr4_fixed1) {
             return x86_err!(BadState, "host CR4 is not valid in VMX operation");
         }
 
         // Get VMCS revision identifier in IA32_VMX_BASIC MSR.
         let vmx_basic = VmxBasic::read();
-        if vmx_basic.region_size as usize != PAGE_SIZE {
-            return x86_err!(Unsupported);
+        if vmx_basic.region_size as usize > PAGE_SIZE {
+            return x86_err!(
+                Unsupported,
+                format_args!(
+                    "unsupported VMX region size: {} bytes",
+                    vmx_basic.region_size
+                )
+            );
         }
         if vmx_basic.mem_type != VmxBasic::VMX_MEMORY_TYPE_WRITE_BACK {
-            return x86_err!(Unsupported);
+            return x86_err!(
+                Unsupported,
+                format_args!("unsupported VMX memory type: {}", vmx_basic.mem_type)
+            );
         }
         if vmx_basic.is_32bit_address {
-            return x86_err!(Unsupported);
+            return x86_err!(Unsupported, "unsupported 32-bit VMX physical address width");
         }
         if !vmx_basic.io_exit_info {
-            return x86_err!(Unsupported);
+            return x86_err!(Unsupported, "VMX lacks I/O exit instruction info");
         }
         if !vmx_basic.vmx_flex_controls {
-            return x86_err!(Unsupported);
+            return x86_err!(Unsupported, "VMX lacks flexible controls");
         }
         self.vmcs_revision_id = vmx_basic.revision_id;
         self.vmx_region = VmxRegion::<H>::new(self.vmcs_revision_id, false)?;
 
         unsafe {
-            // Enable VMX using the VMXE bit.
-            Cr4::write(Cr4::read() | Cr4Flags::VIRTUAL_MACHINE_EXTENSIONS);
+            Cr0::write_raw(cr0_vmx);
+            Cr4::write_raw(cr4_vmx);
             // Execute VMXON.
             vmx::vmxon(self.vmx_region.phys_addr().as_usize() as _).map_err(|err| {
                 x86_err_type!(
@@ -161,6 +167,14 @@ impl<H: X86HostOps> VmxPerCpuState<H> {
         self.vmx_region = unsafe { VmxRegion::<H>::uninit() };
         Ok(())
     }
+}
+
+fn vmx_fixed_control_value(value: u64, fixed0: u64, fixed1: u64) -> u64 {
+    (value | fixed0) & fixed1
+}
+
+fn is_vmx_fixed_control_value_valid(value: u64, fixed0: u64, fixed1: u64) -> bool {
+    (value & fixed0) == fixed0 && (value & !fixed1) == 0
 }
 
 #[cfg(test)]

virtualization/x86_vcpu/src/vmx/vcpu.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use alloc::collections::VecDeque;
+use alloc::{collections::VecDeque, vec::Vec};
 use core::{
     arch::naked_asm,
     fmt::{Debug, Formatter, Result},
@@ -40,9 +40,9 @@ use super::{
     },
 };
 use crate::{
-    X86AccessFlags, X86AccessWidth, X86GuestPhysAddr, X86GuestVirtAddr, X86HostOps,
-    X86HostPhysAddr, X86MsrAddr, X86NestedPageFaultInfo, X86NestedPagingConfig, X86Port,
-    X86VCpuCreateConfig, X86VCpuSetupConfig, X86VcpuError, X86VcpuResult, X86VmExit,
+    X86AccessFlags, X86AccessWidth, X86GuestMemoryRegion, X86GuestPhysAddr, X86GuestVirtAddr,
+    X86HostOps, X86HostPhysAddr, X86MsrAddr, X86NestedPageFaultInfo, X86NestedPagingConfig,
+    X86Port, X86VCpuCreateConfig, X86VCpuSetupConfig, X86VcpuError, X86VcpuResult, X86VmExit,
     ept::GuestPageWalkInfo, host, msr::Msr, regs::GeneralRegisters, restore_host_interrupt_flag,
     x86_real_mode_entry_state, xstate::XState,
 };
@@ -127,6 +127,8 @@ pub struct VmxVcpu<H: X86HostOps> {
     vlapic: EmulatedLocalApic<H>,
     /// Guest CR2 is not saved or restored by VMX hardware.
     guest_cr2: usize,
+    /// Guest RAM regions used to read guest instructions and page tables.
+    guest_memory_regions: Vec<X86GuestMemoryRegion>,
 
     // Extra states
     /// The XState of the VCpu. Both host and guest.
@@ -156,6 +158,7 @@ impl<H: X86HostOps> VmxVcpu<H> {
             pending_events: VecDeque::with_capacity(8),
             vlapic: EmulatedLocalApic::<H>::new(vm_id, vcpu_id),
             guest_cr2: 0,
+            guest_memory_regions: Vec::new(),
             xstate: XState::new(),
             #[cfg(feature = "tracing")]
             guest_regs_exiting: GeneralRegisters::default(),
@@ -519,6 +522,7 @@ impl<H: X86HostOps> VmxVcpu<H> {
         nested_page_table_root: X86HostPhysAddr,
         config: X86VCpuSetupConfig,
     ) -> X86VcpuResult {
+        self.guest_memory_regions = config.guest_memory_regions.clone();
         let paddr = self.vmcs.phys_addr().as_usize() as u64;
         unsafe {
             vmx::vmclear(paddr).map_err(as_axerr)?;
@@ -633,7 +637,7 @@ impl<H: X86HostOps> VmxVcpu<H> {
         is_guest: bool,
         config: X86VCpuSetupConfig,
     ) -> X86VcpuResult {
-        // Intercept NMI and external interrupts.
+        // Intercept external interrupts and use the VMX preemption timer.
         use PinbasedControls as PinCtrl;
 
         use super::vmcs::controls::*;
@@ -643,10 +647,7 @@ impl<H: X86HostOps> VmxVcpu<H> {
             VmcsControl32::PINBASED_EXEC_CONTROLS,
             Msr::IA32_VMX_TRUE_PINBASED_CTLS,
             Msr::IA32_VMX_PINBASED_CTLS.read() as u32,
-            (PinCtrl::NMI_EXITING
-                | PinCtrl::EXTERNAL_INTERRUPT_EXITING
-                | PinCtrl::VMX_PREEMPTION_TIMER)
-                .bits(),
+            (PinCtrl::EXTERNAL_INTERRUPT_EXITING | PinCtrl::VMX_PREEMPTION_TIMER).bits(),
             0,
         )?;
 
@@ -751,8 +752,8 @@ impl<H: X86HostOps> VmxVcpu<H> {
         // VmcsControlNW::CR4_GUEST_HOST_MASK.write(0)?;
         VmcsControl32::CR3_TARGET_COUNT.write(0)?;
 
-        // Pass-through exceptions (except #UD(6)), don't use I/O bitmap, set MSR bitmaps.
-        let exception_bitmap: u32 = 1 << 6;
+        // Pass through guest exceptions. Linux uses #UD (`ud2`) for BUG/WARN handling.
+        let exception_bitmap: u32 = 0;
 
         self.setup_io_bitmap(config)?;
 
@@ -850,18 +851,6 @@ impl<H: X86HostOps> VmxVcpu<H> {
         })()
         .expect("Failed to read guest control register")
     }
-}
-
-// The current VMX APIC-access decode path is used only with Axvisor's
-// identity-mapped guest RAM layout, so the guest physical address is also
-// the host physical address. A non-identity guest memory backend should
-// replace this helper with an explicit GPA-to-HVA translation.
-fn read_guest_phys_u64<H: X86HostOps>(gpa: usize) -> X86VcpuResult<u64> {
-    let mut bytes = [0u8; 8];
-    for (offset, byte) in bytes.iter_mut().enumerate() {
-        *byte = host::read_guest_u8::<H>(X86GuestPhysAddr::from_usize(gpa + offset))?;
-    }
-    Ok(u64::from_le_bytes(bytes))
 }
 
 /// Get ready then vmlaunch or vmresume.
@@ -1321,7 +1310,49 @@ impl<H: X86HostOps> VmxVcpu<H> {
 
     fn read_guest_u8(&self, gva: X86GuestVirtAddr) -> X86VcpuResult<u8> {
         let gpa = self.translate_guest_linear(gva)?;
-        host::read_guest_u8::<H>(gpa)
+        self.read_guest_phys_u8(gpa)
+    }
+
+    fn guest_phys_to_host_virt(&self, gpa: X86GuestPhysAddr) -> X86VcpuResult<usize> {
+        let gpa = gpa.as_usize();
+        self.guest_memory_regions
+            .iter()
+            .find_map(|region| {
+                let start = region.gpa.as_usize();
+                let offset = gpa.checked_sub(start)?;
+                if offset < region.size {
+                    region.hva.as_usize().checked_add(offset)
+                } else {
+                    None
+                }
+            })
+            .ok_or_else(|| {
+                x86_err_type!(
+                    InvalidInput,
+                    format_args!("guest physical address {gpa:#x} is not backed by RAM")
+                )
+            })
+    }
+
+    fn read_guest_phys_u8(&self, gpa: X86GuestPhysAddr) -> X86VcpuResult<u8> {
+        if self.guest_memory_regions.is_empty() {
+            return host::read_guest_u8::<H>(gpa);
+        }
+
+        let hva = self.guest_phys_to_host_virt(gpa)?;
+        Ok(unsafe { core::ptr::read_volatile(hva as *const u8) })
+    }
+
+    fn read_guest_phys_u64(&self, gpa: X86GuestPhysAddr) -> X86VcpuResult<u64> {
+        let mut bytes = [0u8; 8];
+        for (offset, byte) in bytes.iter_mut().enumerate() {
+            let addr = gpa
+                .as_usize()
+                .checked_add(offset)
+                .ok_or(X86VcpuError::InvalidInput)?;
+            *byte = self.read_guest_phys_u8(X86GuestPhysAddr::from_usize(addr))?;
+        }
+        Ok(u64::from_le_bytes(bytes))
     }
 
     fn translate_guest_linear(&self, gva: X86GuestVirtAddr) -> X86VcpuResult<X86GuestPhysAddr> {
@@ -1353,7 +1384,8 @@ impl<H: X86HostOps> VmxVcpu<H> {
         ];
 
         for (level, index) in indexes.into_iter().enumerate() {
-            let entry = read_guest_phys_u64::<H>(table + index * size_of::<u64>())?;
+            let entry =
+                self.read_guest_phys_u64(X86GuestPhysAddr::from(table + index * size_of::<u64>()))?;
             if entry & PRESENT == 0 {
                 return x86_err!(
                     InvalidInput,